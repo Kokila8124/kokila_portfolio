58bfa71d07a6f3ee954b9badba30de614d134f9d
Kokila8124 <kokilaa1709@gmail.com>
2025-03-19 23:20:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling Effect
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
